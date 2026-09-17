8a84107a7913ec30d945c93a7ff537caa54f9bb2
Gustavo-Bento <gfmb94@gmail.com>
2026-09-17 15:21:21 -0300

    refactor(security): reorganize security classes

--- src/main/java/com/bento/springsecurity/config/WebSecurityConfig.java
+++ src/main/java/com/bento/springsecurity/config/WebSecurityConfig.java
@@ -1,7 +1,6 @@
-package com.bento.springsecurity;
+package com.bento.springsecurity.config;
 
 import com.bento.springsecurity.service.SecurityDatabaseService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationProvider;
--- src/main/java/com/bento/springsecurity/controller/WelcomeController.java
+++ src/main/java/com/bento/springsecurity/controller/WelcomeController.java
@@ -1,6 +1,5 @@
-package com.bento.springsecurity;
+package com.bento.springsecurity.controller;
 
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
